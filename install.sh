585be83b904853536ba7a57b18ce9b4f29155749
Zebulun Arendsee <arendsee@iastate.edu>
2014-07-16 00:36:20 -0500

    updated install.sh to work with new zsh files

--- install.sh
+++ install.sh
@@ -3,4 +3,6 @@ cp -i bashrc ~/.bashrc
 cp -i bash_aliases ~/.bash_aliases
 cp -i vimrc ~/.vimrc
 cp -i gitconfig ~/.gitconfig
+cp -i zshrc ~/.zshrc
+cp -i zsh_aliases ~/.zsh_aliases
 source ~/.bashrc
