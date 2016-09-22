47c890f0684ef2e2d1000fc8c288098ac7d40f18
Zebulun Arendsee <arendsee@iastate.edu>
2016-09-21 22:38:52 -0500

    Cleaned up install

--- install/install.sh
+++ install/install.sh
@@ -1,30 +1,41 @@
+# ---------------------------------------------------------
+# --- Haskell stuff
+# Haskell compiler 
+pacman -S ghc
+# Haskell modeule install
+pacman -S cabal-install
+# A library for improved editor handling of Haskell
+pacman -S ghc-mod
+# ctags equivalent for haskell
+pacman -S hasktags
+
+# ---------------------------------------------------------
+# --- shell and xmonad stuff
 # urxvt
 pacman -S rxvt-unicode
-
+# allow zooming etc
+yaourt -S urxvt-resize-font-git
+# run-like command for xmonad, allows partial matching
 pacman -S dmenu
 
-pacman -S cabal-install
-
+# ---------------------------------------------------------
+# --- utilities
 pacman -S zathura zathura-pdf-poppler zathura-ps
-
 # used in vvs alias in bash_aliases
 pacman -S pamixer
-
 # A GUI that allows me to turn my damn head phones on
 # I really need to find a better solution
 pacman -S pavucontrol
-
-pacman -S gdb
-
-pacman -S cgdb
-
 # check battery power
 pacman -S acpi
 # for background images
 pacman -S feh
 
-# allow zooming etc
-yaourt -S urxvt-resize-font-git
-
+# ---------------------------------------------------------
+# --- debugging and coding
+# GNU debugger
+pacman -S gdb
+# Cursed GNU debugger
+pacman -S cgdb
 # needed for autformat in vim for C, C++, Java
 pacman -S astyle
